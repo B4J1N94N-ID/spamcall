38e2a6c2003068cd5c313ea11d072ceb93c212ff
B4J1N94N-ID <73929515+B4J1N94N-ID@users.noreply.github.com>
2020-11-24 11:56:03 +0800

    Create spam.sh

---
+++ spam.sh
@@ -0,0 +1,32 @@
+#!bin/bash
+clear
+
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #ungu
+cy='\033[36;1m' #cyan (biru muda)
+me='\033[31;1m' #merah
+un='\033[37;1m' #ungu #2
+ku='\033[33;1m' #kuning
+toilet -f big -F gay SpamCall
+echo
+echo " ╔═══════════════════•ೋೋ•════════════════════╗"
+echo "   Penulis        : Mr.X969"
+echo "   Whatsapp       : 085397907992"
+echo "   Mr.X969 Popeye Cyber Squad"
+echo " ╚═══════════════════•ೋೋ•════════════════════╝"
+echo
+sleep 0.5
+echo "Masukan Nomor Dengan Benar. Contoh : 823xxxxxxxxx"
+echo
+sleep 1
+read -p "[Masukkan Nomor Hp Korban]>> " nomor
+link="https://id.jagreward.com/member/verify-mobile/$nomor"
+curl -s $link
+sleep 1
+clear
+toilet -f big -F gay Thank You
+sleep 5
+echo "Selamat tinggal :)"
+sleep 1
+exit
